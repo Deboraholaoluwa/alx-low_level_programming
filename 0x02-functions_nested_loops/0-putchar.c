065d243e7bd0a0d250d326b474ceb04c2f63c09f
Deboraholaoluwa <deboraholaoluwa2@gmail.com>
2022-09-13 13:42:32 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,18 +3,18 @@
  * main - Entry point
  * Description: prints 'Putchar \n'
  * Return: Always 0 (success)
- */
+ * */
 int main(void)
 {
-	char str[] = "_putchar\n";
-	int i = 0;
+char str[] = "_putchar\n";
+int i = 0;
 
-	while (str[i] != '\0')
+while (str[i] != '\0')
 	{
-		char c = str[i];
+	char c = str[i];
 		
-		_putchar(c);
-		i++;
+	_putchar(c);
+	i++;
 	}
 	return (0);
 }
